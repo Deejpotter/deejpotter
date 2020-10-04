c15b3f111118d27548752e9897ceab71325f38fd
Deej Potter <deejpotter@gmail.com>
2020-10-04 19:26:37 +1100

    Set up the initial structure.

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$fileLevel = "";
+$title = "Your personal web designer | Deej Potter Designs";
+$description = "I'm a self taught web and game developer and I love what I do.";
+$link = "https://deejpotter.com";
+$image = "images/DeejPotterLogo.png";
+include "templates/header.php"
+?>
+
+<main id="main">
+
+    <section class="jumbotron jumbotron-fluid bg-primary text-dark my-0">
+        <div class="container text-center">
+            <h1 class="display-1">Deej Potter</h1>
+            <p class="lead">Technology is my thing.</p>
+        </div>
+    </section>
+
+</main>
+
+<?php include "templates/footer.php" ?>
---
+++ templates/header.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+	<!-- Meta data -->
+	<title><?php echo $title ?></title>
+	<meta name="description" content="<?php echo $description ?>">
+	<link rel="canonical" href="<?php echo $link ?>">
+	<meta name="robots" content="index, follow">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- OG -->
+	<meta name="og:title" content="<?php echo $title ?>">
+	<meta name="og:description" content="<?php echo $description ?>">
+	<meta name="og:url" content="<?php echo $link ?>">
+	<meta name="og:type" content="website">
+	<meta name="og:image" content="<?php echo $image ?>">
+
+	<!-- Google fonts -->
+	<link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="<?php echo $fileLevel ?>css/bootstrap.css">
+
+	<!-- MAIN CSS -->
+	<link rel="stylesheet" href="<?php echo $fileLevel ?>css/style.css">
+
+</head>
+
+
+<body>
+
+	<!-- Main header for every page -->
+	<nav class="navbar navbar-expand-md">
+		<div class="container">
+			<!-- Brand -->
+			<a class="navbar-brand" href="/">
+				<img src="<?php echo $fileLevel ?>images/DeejPotterLogo.png">
+			</a>
+
+			<!-- Toggler/collapsibe Button -->
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+				<i class="fas fa-bars"></i>
+			</button>
+
+			<!-- Navbar links -->
+			<div class="collapse navbar-collapse" id="collapsibleNavbar">
+				<ul class="navbar-nav nav-fill ml-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo $fileLevel ?>websites">Websites</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo $fileLevel ?>games">Games</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo $fileLevel ?>about">About me</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo $fileLevel ?>contact">Contact me</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
