4b6542d96789b9f647bb86bc8788abed0c15e144
Deej Potter <deejpotter@gmail.com>
2020-10-04 19:54:48 +1100

    Added another section.

--- index.php
+++ index.php
@@ -9,12 +9,21 @@ include "templates/header.php"
 
 <main id="main">
 
+    <!-- Hero section -->
     <section class="jumbotron jumbotron-fluid bg-primary text-dark my-0">
         <div class="container text-center">
             <h1 class="display-1">Deej Potter</h1>
             <p class="lead">Technology is my thing.</p>
         </div>
     </section>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                
+            </div>
+        </div>
+    </div>
 
 </main>
 
