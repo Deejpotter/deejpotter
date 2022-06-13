d48083de951c70a28ba931b0751a47dd3a7855a7
Daniel Potter <daniel.potter@onealphatech.com>
2022-06-13 13:21:27 +1000

    Kept the references I needed and got rid of the rest.

--- _reference/config.js
+++
@@ -1,7 +0,0 @@
-const dotenv = require('dotenv');
-dotenv.config();
-module.exports = {
-  env: process.env.env,
-  connectionString: process.env.connectionString,
-  port: process.env.port
-};
--- _reference/public/js/mobile-menu.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-  var x = document.getElementsByClassName("main-nav")[0];
-  if (x.style.display === "flex") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "flex";
-  }
-}
