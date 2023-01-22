dca4af8c435a57ee688d4aa3fd7c157072b74241
Daniel Potter <daniel.potter@onealphatech.com>
2023-01-22 13:37:42 +1100

    Everything working again and fixed some more styles.

--- src/app/partials/main-nav/main-nav.component.spec.ts
+++ src/app/partials/main-nav/main-nav.component.spec.ts
@@ -1,11 +1,6 @@
 import { LayoutModule } from '@angular/cdk/layout';
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { MainNavComponent } from './main-nav.component';
 
@@ -19,11 +14,6 @@ describe('MainNavComponent', () => {
       imports: [
         NoopAnimationsModule,
         LayoutModule,
-        MatButtonModule,
-        MatIconModule,
-        MatListModule,
-        MatSidenavModule,
-        MatToolbarModule,
       ]
     }).compileComponents();
   }));
