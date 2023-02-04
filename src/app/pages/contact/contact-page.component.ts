1430d4abe6fe935e655c5df8e186ec16bc87f6e6
Daniel Potter <daniel.potter@onealphatech.com>
2023-02-04 21:41:05 +1100

    Updated form

--- src/app/pages/contact/contact-page.component.ts
+++ src/app/pages/contact/contact-page.component.ts
@@ -1,10 +1,4 @@
 import {Component} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {Router} from "@angular/router";
-import {Observable, throwError} from "rxjs";
-import {HttpClient, HttpErrorResponse, HttpParams} from "@angular/common/http";
-import {catchError} from "rxjs/operators";
-
 @Component({
   selector: 'app-contact',
   templateUrl: './contact-page.component.html',
@@ -12,70 +6,7 @@ import {catchError} from "rxjs/operators";
 })
 export class ContactPageComponent {
 
-  constructor(private fb: FormBuilder,
-              private http: HttpClient,
-              private router: Router) {
+  constructor() {
   }
-
-
-  contactForm: FormGroup = this.fb.group({
-    message: ['', Validators.required],
-    email: ['', Validators.email]
-  });
-
-  errorMsg = '';
-
-  closeError() {
-    this.errorMsg = '';
-  }
-
-  submitContactForm(contactFormEntry: any): Observable<any> {
-    const entry = new HttpParams({
-      fromObject: {
-        contactFormEntry
-      }
-    });
-
-    return this.submitEntry(entry);
-  }
-
-  private submitEntry(entry: HttpParams): Observable<any> {
-    return this.http.post(
-      '/contact/index.html',
-      entry.toString(),
-      {
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-        responseType: 'text'
-      }
-    ).pipe(catchError(this.handleError));
-  }
-
-  private handleError(err: HttpErrorResponse) {
-    let errMsg = '';
-
-    if (err.error instanceof ErrorEvent) {
-      errMsg = `A client-side error occurred: ${err.error.message}`;
-    } else {
-      errMsg = `A server-side error occurred. Code: ${err.status}. Message: ${err.message}`;
-    }
-
-    return throwError(() => new Error(errMsg));
-  }
-
-  onSubmit(event: Event) {
-    console.log(event);
-    this.submitContactForm(this.contactForm.value).subscribe(
-      {
-        next: () => {
-          this.contactForm.reset();
-          this.router.navigateByUrl('/');
-        },
-        error: err => {
-          this.errorMsg = err;
-        }
-      }
-    );
-  }
-
 
 }
