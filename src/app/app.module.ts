0013a39d45e3762817a32f35b2e0f3c5cba448e6
Daniel Potter <daniel.potter@onealphatech.com>
2023-04-07 09:53:18 +1000

    Updated build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,31 @@
-import { NgModule } from '@angular/core';
-import {BrowserModule, Title} from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutPageComponent } from './pages/about/about-page.component';
-import { MainNavComponent } from './shared/partials/main-nav/main-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { ContactPageComponent } from './pages/contact/contact-page.component';
-import { TermsComponent } from './pages/terms/terms.component';
-import { PrivacyComponent } from './pages/privacy/privacy.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MainFooterComponent } from './shared/partials/main-footer/main-footer.component';
-import { ProjectsComponent } from './pages/projects/projects.component';
-import { AppsComponent } from './pages/projects/apps/apps.component';
-import { GamesComponent } from './pages/projects/games/games.component';
-import { WebsitesComponent } from './pages/projects/websites/websites.component';
-import { FunComponent } from './pages/projects/fun/fun.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HomeComponent} from './pages/home/home.component';
+import {AboutPageComponent} from './pages/about/about-page.component';
+import {MainNavComponent} from './shared/partials/main-nav/main-nav.component';
+import {LayoutModule} from '@angular/cdk/layout';
+import {ContactPageComponent} from './pages/contact/contact-page.component';
+import {TermsComponent} from './pages/terms/terms.component';
+import {PrivacyComponent} from './pages/privacy/privacy.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {MainFooterComponent} from './shared/partials/main-footer/main-footer.component';
+import {ProjectsComponent} from './pages/projects/projects.component';
+import {AppsComponent} from './pages/projects/apps/apps.component';
+import {GamesComponent} from './pages/projects/games/games.component';
+import {WebsitesComponent} from './pages/projects/websites/websites.component';
+import {FunComponent} from './pages/projects/fun/fun.component';
 import {HttpClientModule} from "@angular/common/http";
-import { BasicSectionComponent } from './shared/templates/sections/basic-section/basic-section.component';
-import { ShadowHeroSectionComponent } from './shared/templates/sections/shadow-hero-section/shadow-hero-section.component';
-import { GradientHeroSectionComponent } from './shared/templates/sections/gradient-hero-section/gradient-hero-section.component';
+import {BasicSectionComponent} from './shared/templates/sections/basic-section/basic-section.component';
+import {
+  ShadowHeroSectionComponent
+} from './shared/templates/sections/shadow-hero-section/shadow-hero-section.component';
+import {
+  GradientHeroSectionComponent
+} from './shared/templates/sections/gradient-hero-section/gradient-hero-section.component';
 
 @NgModule({
   declarations: [
@@ -54,4 +58,5 @@ import { GradientHeroSectionComponent } from './shared/templates/sections/gradie
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/pages/contact/contact-page.component.ts
+++ src/app/pages/contact/contact-page.component.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-contact',
