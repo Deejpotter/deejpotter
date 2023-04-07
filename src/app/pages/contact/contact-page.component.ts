cc1acdfab500853fb60946eff7ed9491b7e670eb
Daniel Potter <daniel.potter@onealphatech.com>
2023-04-07 10:40:27 +1000

    Fix

--- src/app/pages/contact/contact-page.component.ts
+++ src/app/pages/contact/contact-page.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-contact',
