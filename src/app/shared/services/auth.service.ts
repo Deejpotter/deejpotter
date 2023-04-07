0a70151b661646dfbd284ee87df7ae3acebbcf79
Daniel Potter <daniel.potter@onealphatech.com>
2023-04-07 23:18:10 +1000

    Fixed auth.

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import * as netlifyIdentity from 'netlify-identity-widget';
 import {EMPTY, Observable, of} from "rxjs";
-import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root',
