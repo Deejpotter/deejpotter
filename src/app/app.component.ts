39f4cc7a525aa9e1cfd0794301dd8a2c17c6b2a7
Daniel Potter <daniel.potter@onealphatech.com>
2023-04-09 09:24:02 +1000

    Improved the look of the site. Implemented a custom scrollbar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, HostListener, ViewChild} from '@angular/core';
 import {Router, RouterOutlet, NavigationEnd, NavigationStart, ChildrenOutletContexts} from '@angular/router';
 import {filter} from 'rxjs/operators';
 import {routeAnimations} from './shared/animations';
@@ -10,9 +10,26 @@ import {routeAnimations} from './shared/animations';
   animations: [routeAnimations],
 })
 export class AppComponent {
-  animate = false;
+  @ViewChild('contentContainer', { static: false }) contentContainer!: ElementRef;
 
-  constructor(private contexts: ChildrenOutletContexts) {}
+  constructor(private contexts: ChildrenOutletContexts, private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.scrollToTop();
+      });
+  }
+
+  @HostListener('window:popstate', ['$event'])
+  onPopState(event: any) {
+    this.scrollToTop();
+  }
+
+  private scrollToTop() {
+    setTimeout(() => {
+      this.contentContainer.nativeElement.scrollTo({top: 0, behavior: 'smooth'});
+    }, 0);
+  }
 
   getRouteAnimationData() {
     return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
