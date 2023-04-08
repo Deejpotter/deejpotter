3ad5046dc397513dda8c377c384c177a2390907a
Daniel Potter <daniel.potter@onealphatech.com>
2023-04-08 21:35:19 +1000

    Got the route transition animations working better.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,20 @@
 import {Component} from '@angular/core';
-import {Router, RouterOutlet, NavigationEnd} from '@angular/router';
+import {Router, RouterOutlet, NavigationEnd, NavigationStart, ChildrenOutletContexts} from '@angular/router';
 import {filter} from 'rxjs/operators';
-import {fadeInAnimation, slideInAnimation, scaleInAnimation, rotateAnimation} from './shared/animations';
+import {routeAnimations} from './shared/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [slideInAnimation, fadeInAnimation, scaleInAnimation, rotateAnimation],
+  animations: [routeAnimations],
 })
 export class AppComponent {
   animate = false;
 
-  constructor(private router: Router) {
-    this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe(() => {
-        this.animate = true;
-        setTimeout(() => {
-          this.animate = false;
-        }, 300);
-      });
-  }
+  constructor(private contexts: ChildrenOutletContexts) {}
 
+  getRouteAnimationData() {
+    return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
+  }
 }
--- src/app/shared/animations.ts
+++ src/app/shared/animations.ts
@@ -1,31 +1,90 @@
 import {
   trigger,
-  state,
   style,
   transition,
   animate,
+  query,
+  animateChild,
+  group,
 } from '@angular/animations';
 
-export const slideInAnimation = trigger('slideIn', [
-  state('start', style({transform: 'translateX(100%)'})),
-  state('end', style({transform: 'translateX(0)'})),
-  transition('start => end', animate('250ms cubic-bezier(0.35, 0, 0.25, 1)')),
+
+export const routeAnimations = trigger('routeAnimations', [
+  transition('* <=> slideIn', [
+    baseStyle(),
+    ...slideStyles('left'),
+    ...slideAnimation('left'),
+  ]),
+  transition('* <=> fadeIn', [
+    baseStyle(),
+    ...fadeInOutAnimation(),
+  ]),
+  transition('* <=> *', [
+    baseStyle(),
+    ...fadeInOutAnimation(),
+  ]),
 ]);
 
-export const fadeInAnimation = trigger('fadeIn', [
-  state('start', style({opacity: 0})),
-  state('end', style({opacity: 1})),
-  transition('start => end', animate('250ms ease-in')),
-]);
+function baseStyle() {
+  return style({position: 'relative'});
+}
 
-export const scaleInAnimation = trigger('scaleIn', [
-  state('start', style({transform: 'scale(0)'})),
-  state('end', style({transform: 'scale(1)'})),
-  transition('start => end', animate('250ms ease-out')),
-]);
+function slideStyles(direction: string) {
+  return [
+    query(
+      ':enter, :leave',
+      [
+        style({
+          position: 'absolute',
+          top: 0,
+          [direction]: 0,
+          width: '100%',
+        }),
+      ],
+      {optional: true}
+    ),
+    query(':enter', [style({[direction]: '-100%'})], {optional: true}),
+  ];
+}
 
-export const rotateAnimation = trigger('rotate', [
-  state('start', style({transform: 'rotate(0deg)'})),
-  state('end', style({transform: 'rotate(360deg)'})),
-  transition('start => end', animate('500ms ease-in-out')),
-]);
+function slideAnimation(direction: string) {
+  return [
+    query(':leave', animateChild(), {optional: true}),
+    group([
+      query(
+        ':leave',
+        [
+          animate(
+            '200ms ease-out',
+            style({[direction]: '100%', opacity: 0})
+          ),
+        ],
+        {optional: true}
+      ),
+      query(
+        ':enter',
+        [animate('300ms ease-out', style({[direction]: '0%'}))],
+        {optional: true}
+      ),
+      query('@*', animateChild(), {optional: true}),
+    ]),
+  ];
+}
+
+function fadeInOutAnimation() {
+  return [
+    query(':enter, :leave', [style({opacity: 0})], {optional: true}),
+    query(':leave', animateChild(), {optional: true}),
+    group([
+      query(
+        ':leave',
+        [animate('200ms ease-out', style({opacity: 0}))],
+        {optional: true}
+      ),
+      query(':enter', [animate('300ms ease-in', style({opacity: 1}))], {
+        optional: true,
+      }),
+      query('@*', animateChild(), {optional: true}),
+    ]),
+  ];
+}
