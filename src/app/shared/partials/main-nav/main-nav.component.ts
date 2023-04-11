30ed7ea68cf79ffa09db75561ab5c133ea0edad8
Daniel Potter <daniel.potter@onealphatech.com>
2023-04-12 00:29:21 +1000

    Made the site more mobile friendly.

--- src/app/shared/partials/main-nav/main-nav.component.ts
+++ src/app/shared/partials/main-nav/main-nav.component.ts
@@ -1,47 +1,35 @@
 import {Component} from '@angular/core';
-import {dropdownAnimation} from "../../utils/animations";
+import {dropdownAnimation} from '../../utils/animations';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.scss'],
-  animations: [dropdownAnimation()]
+  animations: [dropdownAnimation()],
 })
 export class MainNavComponent {
   isMenuCollapsed = true;
   isFunDropdownOpen = false;
   isWebsitesDropdownOpen = false;
-  isGamesDropdownOpen: boolean = false;
+  isGamesDropdownOpen = false;
 
   toggleNavMenu(): void {
     this.isMenuCollapsed = !this.isMenuCollapsed;
-    this.isFunDropdownOpen = false;
-    this.isWebsitesDropdownOpen = false;
   }
 
   closeNavMenu(): void {
     this.isMenuCollapsed = true;
+  }
+
+  showDropdown(dropdown: string): void {
+    this.isFunDropdownOpen = dropdown === 'fun';
+    this.isWebsitesDropdownOpen = dropdown === 'websites';
+    this.isGamesDropdownOpen = dropdown === 'games';
+  }
+
+  hideDropdowns(): void {
     this.isFunDropdownOpen = false;
+    this.isWebsitesDropdownOpen = false;
     this.isGamesDropdownOpen = false;
-    this.isWebsitesDropdownOpen = false;
   }
-
-  toggleFunDropdown(): void {
-    this.isFunDropdownOpen = !this.isFunDropdownOpen;
-    this.isWebsitesDropdownOpen = false;
-    this.isGamesDropdownOpen = false;
-  }
-
-  toggleGamesDropdown(): void {
-    this.isGamesDropdownOpen = !this.isGamesDropdownOpen;
-    this.isFunDropdownOpen = false;
-    this.isWebsitesDropdownOpen = false;
-  }
-
-  toggleWebsitesDropdown(): void {
-    this.isWebsitesDropdownOpen = !this.isWebsitesDropdownOpen;
-    this.isFunDropdownOpen = false;
-    this.isFunDropdownOpen = false;
-  }
-
 }
