001ac8ae2a5cd306405d97a5943490e5aa1dcd53
Deej Potter <deejpotter@gmail.com>
2024-01-28 10:55:12 +1100

    Add global styles, fonts, metadata, and viewport settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,63 @@
+// Importing global styles
 import "./globals.scss";
+// Importing the Metadata type from Next.js
+import { Metadata, Viewport } from "next"
+// Importing the Fredoka and Nunito fonts from the next/font/google module
 import { Fredoka, Nunito } from "next/font/google";
+// Importing React
 import React from "react";
+// Importing the MainFooter and Navbar components
 import MainFooter from "@/components/MainFooter/MainFooter";
-import { AuthProvider } from "@/contexts/AuthContext";
 import Navbar from "@/components/Navbar/Navbar";
+// Importing the AuthProvider from the AuthContext
+import { AuthProvider } from "@/contexts/AuthContext";
 
+// Initializing the Nunito font with specific options
 const nunito = Nunito({
-  subsets: ["latin"],
-  variable: "--font-nunito",
-});
-const fredoka = Fredoka({
-  subsets: ["latin"],
-  variable: "--font-fredoka",
+  subsets: ["latin"], // Specifies the subsets of the font to load
+  variable: "--font-nunito", // Specifies the CSS variable to assign the font to
 });
 
-export const metadata = {
-  title: "Deej Potter",
-  description: "Deej Potter's portfolio site.",
-  url: "https://deejpotter.com",
-  image: "/images/deejPotterLogo.png",
+// Initializing the Fredoka font with specific options
+const fredoka = Fredoka({
+  subsets: ["latin"], // Specifies the subsets of the font to load
+  variable: "--font-fredoka", // Specifies the CSS variable to assign the font to
+});
+
+// Defining the metadata for the website
+export const metadata: Metadata = {
+  metadataBase: new URL("https://deejpotter.com"),
+  title: "Deej Potter", // The title of the website
+  description: "Deej Potter's portfolio site.", // The description of the website
+  openGraph: {
+    title: "Deej Potter",
+    description: "Deej Potter's portfolio site.",
+    url: "https://deejpotter.com", // The URL of the website
+    images: [
+      {
+        url: "/images/deejPotterLogo.png", // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  manifest: "/manifest.json",
+};
+
+// The viewport data is passed to Next.js to set the viewport meta tag
+export const viewport: Viewport = {
+  themeColor: "#1E9952",
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  colorScheme: 'dark',
 };
 
 // The RootLayout component is the root component that is used to wrap the pages.
@@ -30,13 +69,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    // The root HTML element with language set to English and classes for the fonts
     <html lang="en" className={`${nunito.variable} ${fredoka.variable}`}>
-			{/* The head element is built-in to Next.js and is used to provide metadata to the page.*/}
-			{/* The AuthProvider component is used to provide the authentication context to the components. */}
+      {/* The AuthProvider component is used to provide the authentication context to the components. */}
       <AuthProvider>
+        {/* The body of the HTML document with a custom scrollbar class */}
         <body className="custom-scrollbar">
+          {/* The Navbar component */}
           <Navbar />
+          {/* The main content of the page, which will be the children passed to the RootLayout component */}
           <main>{children}</main>
+          {/* The Footer component */}
           <MainFooter />
         </body>
       </AuthProvider>
