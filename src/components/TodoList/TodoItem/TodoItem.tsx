e3967773311dc52a1d939532a773cd5d6c0c68f5
Deej Potter <deejpotter@gmail.com>
2024-10-18 18:20:24 +1100

    Refactor TodoItem component to use React instead of Angular

--- src/components/TodoList/TodoItem/TodoItem.tsx
+++ src/components/TodoList/TodoItem/TodoItem.tsx
@@ -1,68 +1,3 @@
-/*
- angular code:
- <li class="list-group-item d-flex justify-content-between align-items-center py-1 mb-1" (click)="startEditing()">
- <span *ngIf="!editing">{{ item.text }}</span>
- <ng-container *ngIf="editing">
- <input #editInput *ngIf="editing" class="form-control" [(ngModel)]="updatedText" (blur)="finishEditing()" (keyup.enter)="finishEditing()" />
- <button class="btn btn-primary ml-2" (click)="finishEditing()">
- Save
- </button>
- </ng-container>
- <div>
- <button class="btn btn-warning me-1" (click)="startEditing()" *ngIf="!editing">
- <i class="bi bi-pencil"></i>
- </button>
- <button class="btn btn-danger" (click)="delete.emit(item._id)">
- <i class="bi bi-trash"></i>
- </button>
- </div>
- </li>
-
- 
-  /*
-   Angular code:
-   @Input() item!: TodoItem;
-   @Output() delete: EventEmitter<string> = new EventEmitter();
-   @Output() update: EventEmitter<TodoItem> = new EventEmitter();
-   @ViewChildren('editInput') editInputs!: QueryList<ElementRef>;
-
-   editing = false;
-   updatedText!: string;
-
-   constructor() {
-   }
-
-   ngAfterViewChecked(): void {
-   if (this.editing) {
-   setTimeout(() => {
-   this.focusUpdateInput();
-   }, 0);
-   }
-   }
-
-   startEditing(): void {
-   this.editing = true;
-   this.updatedText = this.item.text;
-   }
-
-   finishEditing(): void {
-   this.editing = false;
-   if (this.updatedText.trim() !== this.item.text.trim()) {
-   this.item.text = this.updatedText;
-   this.update.emit(this.item);
-   }
-   }
-
-   private focusUpdateInput(): void {
-   const inputElement = this.editInputs.first;
-   if (inputElement) {
-   inputElement.nativeElement.focus();
-   }
-   }
-
- nextjs code:
- */
-
 import React, { useState, useRef, useEffect, ReactElement } from "react";
 
 export type TodoItemModel = {
@@ -76,7 +11,7 @@ export type TodoItemModel = {
 };
 
 type TodoItemProps = {
-  item: { id: string; text: string };
+  item: { _id: string; text: string };
   update: (item: { id: string; text: string }) => void;
   delete: (id: string) => void;
 };
