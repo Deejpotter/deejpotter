85e4153c57f4a4c6c07e68c8ba9e6111cce2b14b
Deejpotter <deejpotter@gmail.com>
2026-01-19 21:16:03 +1100

    chore(lint): remove flat ESLint config in favor of legacy .eslintrc.cjs

--- eslint.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ["next/core-web-vitals"],
-  ignorePatterns: [".next/", "out/", "node_modules/"],
-  settings: {},
-  rules: {
-    // repo-specific rule overrides can go here
-  },
-};
